d7de153b480404f1a00b5d980759bd53140c81dc
Maks <maksim.t00@gmail.com>
2021-07-10 21:15:48 +0300

    Init

---
+++ src/main/java/com/rest/cocial/CocialApplication.java
@@ -0,0 +1,13 @@
+package com.rest.cocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CocialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rest/cocial/CocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.cocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
